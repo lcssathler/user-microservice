46e4fb6cb268f8a3cb5a122a85e54e7a835544f4
Lucas Sathler <lcssathler@gmail.com>
2024-01-23 19:45:02 -0300

    user service: saving and publishing message

--- user/src/main/java/com/microservices/user/service/UserService.java
+++ user/src/main/java/com/microservices/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.microservices.user.service;
 
 import com.microservices.user.models.UserModel;
+import com.microservices.user.producers.UserProducer;
 import com.microservices.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,13 +9,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel) {
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
