ceee227c25080f858b14c07731feef25d6500606
Lucas Sathler <lcssathler@gmail.com>
2024-01-23 18:32:48 -0300

    convert payload to json

--- email/src/main/java/com/microservices/email/configs/RabbitMQConfig.java
+++ email/src/main/java/com/microservices/email/configs/RabbitMQConfig.java
@@ -1,5 +1,8 @@
 package com.microservices.email.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.message.ObjectMessage;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +16,11 @@ public class RabbitMQConfig {
     @Bean
     public Queue queue() {
         return new Queue(queue, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
--- email/src/main/java/com/microservices/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/microservices/email/consumers/EmailConsumer.java
@@ -1,12 +1,14 @@
 package com.microservices.email.consumers;
 
+import com.microservices.email.dto.EmailRecordDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.Payload;
 
 @Configuration
 public class EmailConsumer {
     @RabbitListener(queues = "${broker.queue.email.name}")
-    public void listenEmailQueue(String message) {
-        System.out.println(message);
+    public void listenEmailQueue(@Payload EmailRecordDTO emailRecordDTO) {
+        System.out.println(emailRecordDTO.text());
     }
 }
