00887ddb16afe6d73a73aa80752937808c5bb383
Lucas Sathler <lcssathler@gmail.com>
2024-01-22 22:19:49 -0300

    user dto

---
+++ user/src/main/java/com/microservices/user/dto/UserRecordDTO.java
@@ -0,0 +1,10 @@
+package com.microservices.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserRecordDTO(
+        @NotBlank String name,
+        @NotBlank @Email String email
+) {
+}
