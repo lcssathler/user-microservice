fd017e5191784a9fad4edea37b296f454b1c19b1
Lucas Sathler <lcssathler@gmail.com>
2024-01-22 22:20:09 -0300

    user service

---
+++ user/src/main/java/com/microservices/user/service/UserService.java
@@ -0,0 +1,20 @@
+package com.microservices.user.service;
+
+import com.microservices.user.models.UserModel;
+import com.microservices.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public UserModel save(UserModel userModel) {
+        return userRepository.save(userModel);
+    }
+}
